e9e1f49c31cc9885235b3e326055d92e29afc504
joy43 <ssjoy43@gmail.com>
2023-10-21 20:13:53 +0600

    server teachnology adding the site

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port=process.env.PORT ||5000;
 // ------------MIddle ware------------
 app.use(cors());
 app.use(express.json());
-// ----------------
+
 
 
 
@@ -110,7 +110,7 @@ app.delete('/phone/:id', async(req,res)=>{
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(" ph teacnology server. You successfully connected to MongoDB!");
   } finally {
    
 
